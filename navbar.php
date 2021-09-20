5c01e5915869d13d09588bd22326603fc9a14cb1
nandita2000 <76995474+nandita2000@users.noreply.github.com>
2021-09-20 21:21:46 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar -->
+      <nav class="navbar navbar-expand-md">
+      <a class="navbar-brand" href="index.php" style="color : #C0392B;"><b>PEOPLE'S BANK<b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #E05926;"><b>HOME</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #E05926;"><b>CREATE USER</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #E05926;"><b>TRANSFER MONEY</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #E05926;"><b>TRANSACTION HISTORY</b></a>
+              </li>
+          </div>
+       </nav>
